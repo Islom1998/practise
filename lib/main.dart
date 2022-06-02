4af00b8287c334e45bdaba221d6be9a4baae618b
Islom1998 <103881528+Islom1998@users.noreply.github.com>
2022-06-02 12:31:54 +0500

    final

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
